279d70b6b6755598cf79356cbb3ae15f1021afe9
ebrarkesici <141298599+ebrarkesici@users.noreply.github.com>
2024-05-19 13:53:59 +0300

    Update authorization.go
    Removed unused dependencies and commented out CheckAuth function for cleaner code.

--- internal/application/presentation/port/command/authorization.go
+++ internal/application/presentation/port/command/authorization.go
@@ -1,14 +1,8 @@
 package application
 
-import (
-	"context"
-
-	pb "github.com/octoposprime/op-be-shared/pkg/proto/pb/authorization"
-)
-
 // AuthorizationCommandPort is a port for Hexagonal Architecture Pattern.
 // It is used to communicate with the application layer.
 type AuthorizationCommandPort interface {
 	// CheckAuth returns true if the user is authorized to perform the action.
-	CheckAuth(ctx context.Context, authRequest *pb.AuthRequest) (*pb.AuthResponse, error)
+	//CheckAuth(ctx context.Context, authRequest *pb.AuthRequest) (*pb.AuthResponse, error)
 }
