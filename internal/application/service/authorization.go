3e5c049f7f9a534a93853e52914e33da15722495
semanurgüçlü <smnrguclu06@gmail.com>
2024-05-19 17:57:10 +0300

    Authorization.go file is disabled

--- internal/application/service/authorization.go
+++ internal/application/service/authorization.go
@@ -1,12 +1,6 @@
 package application
 
-import (
-	"context"
-
-	pb "github.com/octoposprime/op-be-shared/pkg/proto/pb/authorization"
-)
-
 // CheckAuth returns true if the user is authorized to perform the action.
-func (a *Service) CheckAuth(ctx context.Context, authRequest *pb.AuthRequest) (*pb.AuthResponse, error) {
-	return a.ServicePort.CheckAuth(ctx, authRequest)
-}
+// func (a *Service) CheckAuth(ctx context.Context, authRequest *pb.AuthRequest) (*pb.AuthResponse, error) {
+//     return a.ServicePort.CheckAuth(ctx, authRequest)
+// }
